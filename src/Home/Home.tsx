6ed6f74441a83b7f810778d25721bce627674d67
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-02-16 15:39:44 +0000

    Embedded curiosity weather image

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,7 +1,12 @@
+import React from 'react'
+
+
 function Home() {
   return (
-    <div>home page</div>
-  )
+    <div>home page
+    <iframe src='https://mars.nasa.gov/layout/embed/image/mslweather/' width='100%' height='600'  scrolling='no' frameborder='0'></iframe>
+    </div>
+    ) 
 }
 
 export { Home }
