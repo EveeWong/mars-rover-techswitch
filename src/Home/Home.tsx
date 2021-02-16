d7c05fbbfcdd713c0f9ad6a9564488e40663484d
ZOO\MarAza <Mariam.Azarian@softwire.com>
2021-02-16 16:10:01 +0000

    created the mobile nav bar with some styling

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,8 +1,10 @@
 import { PicOfTheDay } from '../PicOfTheDay/PicOfTheDay';
+import { MobileNavbar } from '../MobileNavbar/MobileNavbar';
 
 function Home() {
   return (
     <div>
+      <MobileNavbar />
       Home page
       <PicOfTheDay />
     </div>
--- src/MobileNavbar/MobileNavbar.tsx
+++ src/MobileNavbar/MobileNavbar.tsx
@@ -1,7 +1,27 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import './MobileNavbar.scss';
 
-function MobileNavbar {
+function MobileNavbar() {
+    return (
+        <div>
+            <button type="button" className="mobile_nav_toggle" id="mobile_nav_toggle" onClick={toggleMobileNav}>
+                X
+            </button>
 
+            <nav className="mobile_nav" id="mobile_nav">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Rovers</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+function toggleMobileNav() {
+    var navbar = document.getElementById("mobile_nav");
+    navbar.classList.toggle("mobile_nav_visible");
 }
 
 export { MobileNavbar };
