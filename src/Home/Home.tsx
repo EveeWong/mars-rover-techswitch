822d11b1354cf6c6d45cd9ca1bbd5aaf12b99577
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-02-17 11:13:41 +0000

    abstracted weather component

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,11 +1,12 @@
 import { PicOfTheDay } from './PicOfTheDay/PicOfTheDay';
+import { Weather } from './Weather/Weather';
 
 function Home() {
   return (
     <div>
       Home page
       <PicOfTheDay />
-      <iframe src='https://mars.nasa.gov/layout/embed/image/mslweather/' width='100%' height='650'  scrolling='no' frameborder='0'></iframe>
+      <Weather />
     </div>
   )
 }
