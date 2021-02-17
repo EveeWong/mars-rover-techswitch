6e7a566da192380316a81cfaa37ddc2ddc364448
Emily Patsko <Emily.Patsko@softwire.com>
2021-02-17 15:43:54 +0000

    Wrapped the tests in a <BrowserRouter> tag.

--- src/Home/Weather/Weather.tsx
+++ src/Home/Weather/Weather.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Weather() {
     return <div>
-        <iframe className='weatherTable' src='https://mars.nasa.gov/layout/embed/image/mslweather/' width='100%' height='650'  scrolling='no' frameborder='0'></iframe>
+        <iframe className='weatherTable' src='https://mars.nasa.gov/layout/embed/image/mslweather/' width='100%' height='650'  scrolling='no'></iframe>
     </div>
 }
 
