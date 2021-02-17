0f6bfc45c7eeecf55516f202c95b9d47a647a929
Emily Patsko <Emily.Patsko@softwire.com>
2021-02-17 15:49:16 +0000

    Amended the mobile navbar test to wrap it in a browser router tag.

--- src/MobileNavbar/MobileNavbar.test.tsx
+++ src/MobileNavbar/MobileNavbar.test.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { MobileNavbar } from './MobileNavbar';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders the MobileNavbar component without errors', () => {
-  render(<MobileNavbar/>);
+  render(
+    <BrowserRouter>
+      <MobileNavbar/>
+    </BrowserRouter>
+  );
 });
