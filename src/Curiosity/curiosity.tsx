5063dcf55976d504a014d78dd5c842950487aca4
RoshniCh <roshni.chandrasekar@softwire.com>
2021-02-17 10:23:11 +0000

    review comments for curiosity and spirit

--- src/Curiosity/curiosity.tsx
+++ src/Curiosity/curiosity.tsx
@@ -1,33 +1,32 @@
-  
+
 
 import React from 'react';
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import './curiosity.scss';
 
-function Curiosity() {  
+function Curiosity() {
     const [curiosityPhotoData, setCuriosityPhotoData] = useState(null);
     console.log(process.env.REACT_APP_NASA_API_KEY)
     useEffect(() => {
         fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=0&api_key=${process.env.REACT_APP_NASA_API_KEY}`)
-        .then(response => response.json())
-        .then(data => setCuriosityPhotoData(data))
-    },[]);
-    
+            .then(response => response.json())
+            .then(data => setCuriosityPhotoData(data))
+    }, []);
+
     if (!curiosityPhotoData) {
         return <div>Waiting for data!</div>
-        }
-    return (                
-        <div>   
+    }
+    return (
+        <div>
             <div>Rover : {curiosityPhotoData.photos[0].rover.name} </div>
             <div>Camera : {curiosityPhotoData.photos[0].camera.full_name} </div>
-            <div>  
-                 {/* {myPhotos.photos.map(photo => <img src={photo.img_src}/>)} */}
-                  <img className="img-curiosity-main" src={curiosityPhotoData.photos[0].img_src}/>
+            <div>
+                <img className="img-curiosity-main" src={curiosityPhotoData.photos[0].img_src} />
             </div>
             <div>Earth date : {curiosityPhotoData.photos[0].earth_date} </div>
             <div>Sol : {curiosityPhotoData.photos[0].sol} </div>
         </div>
-  )
+    )
 }
 
 export { Curiosity }
--- src/Spirit/spirit.tsx
+++ src/Spirit/spirit.tsx
@@ -1,32 +1,31 @@
-  
+
 import React from 'react';
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import './spirit.scss';
 
-function Spirit() {  
+function Spirit() {
     const [spiritPhotoData, setSpiritPhotoData] = useState(null);
     useEffect(() => {
         fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=1&api_key=${process.env.REACT_APP_NASA_API_KEY}`)
-        .then(response => response.json())
-        .then(data => setSpiritPhotoData(data))
-    },[]);
-    
+            .then(response => response.json())
+            .then(data => setSpiritPhotoData(data))
+    }, []);
+
     if (!spiritPhotoData) {
         return <div>Waiting for data!</div>
-        }
-    return (                
-        <div>   
+    }
+    return (
+        <div>
             <div>Rover : {spiritPhotoData.photos[0].rover.name} </div>
             <div>Camera : {spiritPhotoData.photos[0].camera.full_name} </div>
-            <div>  
-                 {/* {myPhotos.photos.map(photo => <img src={photo.img_src}/>)} */}
-                  <img className="img-spirit-main" src={spiritPhotoData.photos[0].img_src}/>
+            <div>
+                <img className="img-spirit-main" src={spiritPhotoData.photos[0].img_src} />
             </div>
             <div>Earth date : {spiritPhotoData.photos[0].earth_date} </div>
             <div>Sol : {spiritPhotoData.photos[0].sol} </div>
 
         </div>
-  );
+    );
 }
 
 export { Spirit }
