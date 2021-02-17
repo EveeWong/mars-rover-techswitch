f944c21bcd7a2b061fa5b3ede36d0c710299f884
Chloe Jones <Chloe.Jones@softwire.com>
2021-02-17 11:04:09 +0000

    set several fonts and colors, needs to have the NASA font imported at a later stage.

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,6 +1,14 @@
+import '../Fonts.scss'; 
 function Home() {
   return (
-    <div>home page</div>
+    <div>
+    <h1>h1 NASA FONT</h1>
+    <h2>h2 home page</h2>
+    <h3>h3 home page</h3>
+    <h4>h4 home page</h4>
+    <h5>h5 home page</h5>
+    <p>p paragraph</p>
+    </div>
   )
 }
 
