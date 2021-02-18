c97eb9cecb1230b3193e2309ea329b9cb3f636cc
Chloe Jones <Chloe.Jones@softwire.com>
2021-02-18 10:43:09 +0000

    made changes such as deleted comments, removing unwanted code and indenting

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -6,13 +6,6 @@ import { Weather } from './Weather/Weather';
 function Home() {
   return (
     <div> 
-          <h1>h1 NASA FONT</h1>
-          <h2>h2 home page</h2>
-          <h3>h3 home page</h3>
-          <h4>h4 home page</h4>
-          <h5>h5 home page</h5>
-          <p>p paragraph</p>
-
       Home page
       <PicOfTheDay />
       <Weather />
--- src/RoverSelection/RoverSelection.tsx
+++ src/RoverSelection/RoverSelection.tsx
@@ -5,22 +5,22 @@ import { Link } from 'react-router-dom';
 function RoverSelection() {
 
   return (
-     <div>
+    <div>
       <h1> Rover selection page </h1>
-        <div className='CuriosityRover'>   
+      <div className='CuriosityRover'>
         <RoverSummary rover={roverData.curiosity} />
         <Link to='/rovers/curiosity'> Explore Curiosity Rover! </Link>
-        </div>
-        <div className='OpportunityRover'>   
+      </div>
+      <div className='OpportunityRover'>
         <RoverSummary rover={roverData.opportunity} />
         <Link to='/rovers/opportunity'> Explore Opportunity Rover! </Link>
-        </div>
-        <div className='SpiritRover'>   
+      </div>
+      <div className='SpiritRover'>
         <RoverSummary rover={roverData.spirit} />
         <Link to='/rovers/spirit'> Explore Spirit Rover! </Link>
-        </div>
-      </div> 
-    )
-  }
-  
-  export {RoverSelection}
+      </div>
+    </div>
+  )
+}
+
+export { RoverSelection }
