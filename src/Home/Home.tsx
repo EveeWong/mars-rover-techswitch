2f3501c633c08acbd750765e6d5cc36227efae63
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-02-18 10:18:33 +0000

    created divs for the pic of the day component, added margins

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,10 +1,11 @@
 import { PicOfTheDay } from './PicOfTheDay/PicOfTheDay';
 import { Weather } from './Weather/Weather';
+import './Home.scss'
 
 function Home() {
   return (
     <div>
-      Home page
+      <h1>MotherBase</h1>
       <PicOfTheDay />
       <Weather />
     </div>
