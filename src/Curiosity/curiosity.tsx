9d7958bc97d5a8f303aee0248fc81c4ad074d7a7
RoshniCh <roshni.chandrasekar@softwire.com>
2021-02-18 15:37:21 +0000

    curiosity rover date change with error validation

--- src/Curiosity/curiosity.tsx
+++ src/Curiosity/curiosity.tsx
@@ -11,7 +11,6 @@ function Curiosity() {
         fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?&earth_date=${submitDate}&camera=navcam&api_key=${process.env.REACT_APP_NASA_API_KEY}`)
             .then(response => response.json())
             .then(data => {
-                // deal with the case that there is no data
                 setCuriosityPhotoData(data)
             })
     }, [submitDate]);
@@ -23,16 +22,29 @@ function Curiosity() {
     if (!curiosityPhotoData) {
         return <div>Waiting for data!</div>
     }
-    // verify some data exists or return a page that says no data for this date
+
     let curiosityPhoto = curiosityPhotoData.photos.slice(0, 6);
+    let photoAvailableBoolean = false;
+    let displayDataJsx
+    if (curiosityPhoto.length > 0) {
+        photoAvailableBoolean = true;
+    }
+
+    if (photoAvailableBoolean == true) {
+        displayDataJsx = <RoverPhoto photoData={curiosityPhoto} />
+    } else {
+        displayDataJsx = <div>No photos available for this date. Please choose a different date. Curiosity rover has been on Mars from 2012-08-06 till today. Some dates may not have images</div>
+    }
+
     return (
         <div>
-            <label>
+            <h1>Curiosity Rover</h1>
+            <label className="rover-date-top-padding">
                 Date
-                <input type="date" name="searchDate" onChange={e => setSearchDate(e.target.value)} />
-                <button onClick={() => searchForNewDate()}>Search</button> 
+                <input className="rover-input-date" type="date" name="searchDate" onChange={e => setSearchDate(e.target.value)} />
+                <button className="rover-input-date" onClick={() => searchForNewDate()}>Search</button>
             </label>
-            <RoverPhoto photoData={curiosityPhoto} />
+            {displayDataJsx}
         </div>
     )
 }
