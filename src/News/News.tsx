db6822a6c673b9ca7d916a3b400700269a66d9a8
ZOO\MarAza <Mariam.Azarian@softwire.com>
2021-02-18 14:47:12 +0000

    news nasaMars tweet embeded

--- src/News/News.tsx
+++ src/News/News.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import '../Fonts.scss'; 
 import { MobileNavbar } from '../MobileNavbar/MobileNavbar';
+import { Timeline } from 'react-twitter-widgets';
+import './News.scss';
 
 function News (){
     return(
-        <div>
+        <div className="news_container">
             <MobileNavbar />
             <h1>News</h1>
+            <Timeline 
+                dataSource={{
+                    sourceType: 'profile',
+                    screenName: 'NASAMars'
+                }}
+            />
         </div>
         
     )
