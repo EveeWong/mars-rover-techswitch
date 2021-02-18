ad6d636ba0fa02761b466a1845db59c0686f9ece
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-02-18 17:01:41 +0000

    minor navbar styling

--- src/Home/Weather/Weather.tsx
+++ src/Home/Weather/Weather.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Weather() {
     return <div className='weatherContainer'>
-        <iframe className='weatherTable' src='https://mars.nasa.gov/layout/embed/image/mslweather/' width='100%' height='600'  scrolling='no' frameborder='0'></iframe>
+        <iframe className='weatherTable' src='https://mars.nasa.gov/layout/embed/image/mslweather/' width='100%' height='600'  scrolling='no' ></iframe>
     </div>
 }
 
