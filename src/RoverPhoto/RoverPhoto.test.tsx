3553f8316e25abeeba29e3d154b4286b6ecad9d9
RoshniCh <roshni.chandrasekar@softwire.com>
2021-02-19 10:57:08 +0000

    review changes for testing

--- src/RoverPhoto/RoverPhoto.test.tsx
+++ src/RoverPhoto/RoverPhoto.test.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { RoverPhoto } from './RoverPhoto';
 
+const roverPhotoData = [{
+    img_src : "https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/03032/opgs/edr/ncam/NLB_666664401EDR_F0861456NCAM00278M_.JPG",
+    camera:{full_name:"Navigation Camera"},
+    earth_date:"2021-02-15",
+    sol: "3032"}]
+
 test('renders the rover photo component without errors', () => {
-  render(<RoverPhoto />);
+  render(<RoverPhoto photoData={roverPhotoData}/>);
 });
